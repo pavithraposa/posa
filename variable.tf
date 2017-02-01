1e5122f3a69edf3a9fbc7fb4ff24186420463bbe
pavithraposa <posapavithra@gmail.com>
2017-02-01 18:36:42 +0530

    Add files via upload

--- variable.tf
+++ variable.tf
@@ -1,22 +1,23 @@
  
-provider "aws" {
-    region = "${var.aws_region}"
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-}
+variable "aws" {
+    region = "${us-west-2}"
+    access_key = "${AKIAJOY7V2RW5D6MEGWA}"
+    secret_key = "${
+1Ts3vBwD0f4oLTh6pIovlRjHKPL/ikkVlwBR79ga}"
+ } 
 resource "aws_instance" "web" {
   connection {
     user = "ubuntu"
     }
-  subnet_id = "${var.subnet_id}"
-  instance_type = "${var.instance_type}"
+  subnet_id = "${subnet-86759fcf}"
+  instance_type = "${t2.micro}"
 
-  ami = "${var.aws_ami}"
+  ami = "${ami-30e65350}"
 
-  key_name = "${var.key_name}"
+  key_name = "${chef}"
 
 
   tags {
-    "Name" = "${var.instance_name}"
+    "Name" = "${pavithra}"
   }} 
 
